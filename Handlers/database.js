3e0dbe43661f7bfd012e60bf96afd6cd3dd5ae71
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-07 13:31:39 +0100

    Setup user key authorization

--- Handlers/database.js
+++ Handlers/database.js
@@ -10,3 +10,45 @@ const connectionPool = mysql.createPool({
 });
 
 exports.connectionPool = connectionPool;
+
+exports.authUser = function authUser(steamID,key){
+	connectionPool.query(`SELECT * FROM UserKey WHERE Key=?`,[key], (err,rows) => {
+		if (rows.length < 1){
+			return {isValid : false, msg : "Invalid Key"}
+		}else if (rows.lenth > 1){
+			return {isValid : false, msg : "Duplicate key"}
+		}else{
+			let validAttempt = false;
+			switch (rows[0].AuthLevel){
+				case 0:
+					// Self only
+					validAttempt = checkLink(steamID,rows[0].LinkedUser);
+					break;
+				case 1:
+					// Global key
+					validAttempt = true;
+					break;
+				default:
+					validAttempt = false;
+					break;
+			}
+
+			return validAttempt;
+		}
+	});
+}
+
+async function checkLink(steamID,User){
+	let promise = new Promise((resolve,reject) => {
+		connectionPool.query(`SELECT Steam_ID FROM User WHERE ID=${linkedUser}`, (err,rows) => {
+			if (steamID === rows[0].Steam_ID){
+				resolve(true);
+			}else{
+				resolve(false);
+			}
+		})
+	});
+
+	let result = await promise;
+	return result;
+}
