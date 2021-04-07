1e69c9fee1a18a7f6fbd813db4010e6591f849b9
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-07 02:50:05 +0100

    Updated connection name

--- Handlers/database.js
+++ Handlers/database.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 const config = require('./../config.json');
 
-exports.pool = connectionPool;
-
-var connectionPool = mysql.createPool({
+const connectionPool = mysql.createPool({
 	connectionLimit : 30,
 	host : config.databaseInfo.host,
 	user : config.databaseInfo.user,
 	password : config.databaseInfo.password,
 	database : config.databaseInfo.database
 });
+
+exports.connectionPool = connectionPool;
