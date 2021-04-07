e2370714b42df6039f8036d9afc71624d5490b27
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-07 13:52:22 +0100

    Reworked promises

--- Handlers/database.js
+++ Handlers/database.js
@@ -12,34 +12,36 @@ const connectionPool = mysql.createPool({
 exports.connectionPool = connectionPool;
 
 exports.authUser = function authUser(steamID,key){
-	connectionPool.query(`SELECT * FROM UserKey WHERE AuthKey=?`,[key], (err,rows) => {
-		if (rows.length < 1){
-			return {isValid : false, msg : "Invalid Key"}
-		}else if (rows.lenth > 1){
-			return {isValid : false, msg : "Duplicate key"}
-		}else{
-			let validAttempt = false;
-			switch (rows[0].AuthLevel){
-				case 0:
-					// Self only
-					validAttempt = checkLink(steamID,rows[0].LinkedUser);
-					break;
-				case 1:
-					// Global key
-					validAttempt = true;
-					break;
-				default:
-					validAttempt = false;
-					break;
+	return new Promise((resolve,reject) => {
+		connectionPool.query(`SELECT * FROM UserKey WHERE AuthKey=?`,[key], async (err,rows) => {
+			if (rows.length < 1){
+				return {isValid : false, msg : "Invalid Key"}
+			}else if (rows.lenth > 1){
+				return {isValid : false, msg : "Duplicate key"}
+			}else{
+				let validAttempt = { isValid : false, msg : null};
+				switch (rows[0].AuthLevel){
+					case 0:
+						// Self only
+						validAttempt.isValid = await checkLink(steamID,rows[0].LinkedUser);
+						break;
+					case 1:
+						// Global key
+						validAttempt.isValid = true;
+						break;
+					default:
+						validAttempt.isValid = false;
+						validAttempt.msg = "Invalid key.";
+						break;
+				}
+				resolve(validAttempt);
 			}
-
-			return validAttempt;
-		}
+		});
 	});
 }
 
-async function checkLink(steamID,User){
-	let promise = new Promise((resolve,reject) => {
+function checkLink(steamID,User){
+	return new Promise((resolve,reject) => {
 		connectionPool.query(`SELECT Steam_ID FROM User WHERE ID=${User}`, (err,rows) => {
 			if (steamID === rows[0].Steam_ID){
 				resolve(true);
@@ -48,7 +50,4 @@ async function checkLink(steamID,User){
 			}
 		})
 	});
-
-	let result = await promise;
-	return result;
 }
