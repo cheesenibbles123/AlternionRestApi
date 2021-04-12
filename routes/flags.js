82c30d237852ed2b687326cc65f97002151d3e2d
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-12 15:19:38 +0100

    Added initial flags' '

--- routes/flags.js
+++ routes/flags.js
@@ -1,16 +1,52 @@
 const db = require('./../Handlers/database.js');
 const responses = require('./../Handlers/responses.js');
 
+
 module.exports = {
 	path: "/flags",
 	route: (router) => {
 
 		router.get('/', async (req, res) => {
-		  if (req.query.steamID && req.query.key){
+		  if (req.query.steamID && req.query.key && req.query.public){
 		    let validKey = await db.authUser(req.query.steamID,req.query.key);
 		    if (validKey.isValid){
 		      let steamID = parseInt(req.query.steamID);
 		      if (!isNaN(steamID)){
+
+		      	let obj;
+		      	
+		      	if (parseInt(req.query.public) === 1){
+		      		db.connectionPool.query(`SELECT Name, Display_Name FROM Flag WHERE Limited!=1`, (err,rows) => {
+
+						if (rows.length < 1){
+							obj = "No Flags found.";
+						}else{
+							obj = {};
+							for (let i = 0; i < rows.length; i++){
+								obj[`${rows[i].Name}`] = rows[i].Display_Name;
+							}
+						}
+
+						responses.returnSuccess(res,obj);
+					});
+		      	}else{
+		      		db.connectionPool.query(`SELECT Team_ID from User where Steam_ID=?`,[steamID], (err,rows1) => {
+		      			console.log(`(SELECT Flag.Name, Flag.Display_Name FROM LimitedFlags INNER JOIN User ON User_ID = User.ID INNER JOIN Flag ON Allowed_Flag_ID = Flag.ID WHERE User.Steam_ID='${steamID}') UNION (SELECT Name, Display_Name FROM Cannon WHERE Team_ID=${rows1[0].Team_ID} AND IF ( ${rows1[0].Team_ID} != 0, 1, 0) = 1 )`)
+						db.connectionPool.query(`(SELECT Flag.Name, Flag.Display_Name FROM LimitedFlags INNER JOIN User ON User_ID = User.ID INNER JOIN Flag ON Allowed_Flag_ID = Flag.ID WHERE User.Steam_ID=?) UNION (SELECT Name, Display_Name FROM Cannon WHERE Team_ID=${rows1[0].Team_ID} AND IF ( ${rows1[0].Team_ID} != 0, 1, 0) = 1 )`,[steamID], (err,rows) => {
+
+							if (rows.length < 1){
+								obj = "No Flags found.";
+							}else{
+								obj = {};
+								for (let i = 0; i < rows.length; i++){
+									obj[`${rows[i].Name}`] = rows[i].Display_Name;
+								}
+							}
+
+							responses.returnSuccess(res,obj);
+						});
+					});
+		      	}
 
 		      }else{
 		        responses.returnError(res,"Please check your inputs.");
