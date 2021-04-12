de0828357fb88a817828f0283f8e421426bf546c
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-12 04:11:14 +0100

    Setup initial file structure

---
+++ routes/badges.js
@@ -0,0 +1,28 @@
+const db = require('./../Handlers/database.js');
+const responses = require('./../Handlers/responses.js');
+
+module.exports = {
+	path: "/badges",
+	route: (router) => {
+
+		router.get('/', async (req, res) => {
+		  if (req.query.steamID && req.query.key){
+		    let validKey = await db.authUser(req.query.steamID,req.query.key);
+		    if (validKey.isValid){
+		      let steamID = parseInt(req.query.steamID);
+		      if (!isNaN(steamID)){
+
+		      }else{
+		        responses.returnError(res,"Please check your inputs.");
+		      }
+		    }else{
+		      responses.returnError(res,validKey.msg);
+		    }
+		  }else{
+		    responses.returnError(res,"Missing parameters.");
+		  }
+		});
+		
+		return router;
+	}
+}
