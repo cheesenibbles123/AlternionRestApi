e26fa9f6646468d70e14cb4ed6693c6330b536b6
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-12 16:59:56 +0100

    Added TL auth

--- Handlers/database.js
+++ Handlers/database.js
@@ -40,11 +40,57 @@ exports.authUser = function authUser(steamID,key){
 	});
 }
 
+exports.authTeamLeader = function authTeamLeader(tlSteamID, affectedSteamID, key){
+	return new Promise((resolve,reject) => {
+		connectionPool.query(`SELECT * FROM UserKey WHERE AuthKey=?`,[key], async (err,rows) => {
+			if (rows.length < 1){
+				reject({isValid : false, msg : "Invalid Key"});
+			}else if (rows.length > 1){
+				reject({isValid : false, msg : "Duplicate key"});
+			}else{
+				let validAttempt = { isValid : false, msg : null};
+				switch (rows[0].AuthLevel){
+					case 1:
+						// Global key
+						validAttempt.isValid = true;
+						break;
+					case 2:
+						// Team Leader
+						validAttempt.isValid = await checkTeam(affectedSteamID,tlSteamID,rows[0].LinkedUser);
+					default:
+						validAttempt.isValid = false;
+						validAttempt.msg = "Invalid key.";
+						break;
+				}
+				resolve(validAttempt);
+			}
+		});
+	});
+}
+
 function checkLink(steamID,User){
 	return new Promise((resolve,reject) => {
 		connectionPool.query(`SELECT Steam_ID FROM User WHERE ID=${User}`, (err,rows) => {
 			if (steamID === rows[0].Steam_ID){
 				resolve(true);
+			}else{
+				resolve(false);
+			}
+		})
+	});
+}
+
+function checkTeam(targetSteamID,executorSteamID,Team){
+	return new Promise((resolve,reject) => {
+		connectionPool.query(`SELECT Team_ID,Team_Leader FROM User WHERE Steam_ID=?`,[executorSteamID], (err,rows) => {
+			if (rows[0].Team_Leader !== 0 && rows[0].Team_ID === Team){
+				connectionPool.query(`SELECT Team_ID,Team_Leader FROM User WHERE Steam_ID=?`,[targetSteamID], (err,rows2) => {
+					if (rows2[0].Team_ID === rows[0].Team_ID){
+						resolve(true);
+					}else{
+						resolve(false);
+					}
+				})
 			}else{
 				resolve(false);
 			}
